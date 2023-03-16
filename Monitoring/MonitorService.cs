5664c321aa2fe21fa7882f616f862e977fc69de0
Christian Lindholm <christianlindholm@jubii.dk>
2023-03-16 13:17:08 +0100

    Initial commit

---
+++ Monitoring/MonitorService.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Serilog;
+
+namespace Monitoring;
+
+public static class MonitorService
+{
+    public static readonly string ServiceName = Assembly.GetCallingAssembly().GetName().Name ?? "Unknown";
+    public static TracerProvider? TracerProvider;
+    public static ActivitySource ActivitySource = new ActivitySource(ServiceName);
+    public static ILogger Log => Serilog.Log.Logger;
+    static MonitorService()
+    {
+        //OpenTelemetry
+        TracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddZipkinExporter()
+            .AddConsoleExporter()
+            .AddSource(ActivitySource.Name)
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(ServiceName))
+            .Build();
+        
+        //Serilog
+        Serilog.Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .WriteTo.Console()
+            .WriteTo.Seq("http://localhost:5341")
+            .CreateLogger();
+    }
+}
---
+++ Monolith/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Monolith;
+using Serilog;
+
+public class Program
+{
+    public static void Main()
+    {
+        var game = new Game();
+        for (int i = 0; i < 100; i++)
+        {
+            game.Start();
+        }
+        Console.WriteLine("Finished");
+        Log.CloseAndFlush();
+    }
+}
